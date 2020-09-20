bb4c8b963f03b2bef8bb2bd43ee06e86db742f9f
Glanfaloth Cevnas <tyl15@mails.tsinghua.edu.cn>
2020-09-20 04:25:42 +0200

    Add Splash Screen

---
+++ app/src/main/java/com/huawei/mlkit/example/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.huawei.mlkit.example;
+
+import android.os.Bundle;
+import com.huawei.hackzurich.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
